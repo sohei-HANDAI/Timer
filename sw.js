dbaff7581965608f41f8e27f3ba259cdf09d3212
sohei-HANDAI <sohei.ide.osaka.u@gmail.com>
2025-01-08 00:52:04 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "plank-timer-cache-v1";
+const urlsToCache = [
+    "./",
+    "./index.html",
+    "./styles.css",
+    "./script.js",
+    "./manifest.json",
+    "https://www.soundjay.com/button/beep-07.wav"
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) =>
+            Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            )
+        )
+    );
+});
